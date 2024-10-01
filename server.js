31bb0265fd973c6da88fb3195cdbb33160160b07
zukahm2005 <145833253+zukahm2005@users.noreply.github.com>
2024-10-01 15:01:19 +0700

    Initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/playgame', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
